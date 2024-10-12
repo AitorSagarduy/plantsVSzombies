825e2017f292b9aa0e12f8426497a81b544359bd
r2213432 <jhojanmartinezmayta@gmail.com>
2024-10-12 14:47:41 +0200

    Añadido imagen de plantas
    Ahora se muestra correctamente la imagen de la planta como icono tanto
    en la lista como en los botones que son parte del patio

--- PvZ/src/main/Simulacionv1.java
+++ PvZ/src/main/Simulacionv1.java
@@ -4,10 +4,19 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Component;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
 import javax.swing.DefaultListCellRenderer;
+import javax.swing.DefaultListModel;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -16,6 +25,7 @@ import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JScrollPane;
+import javax.swing.ListModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
@@ -31,24 +41,63 @@ public class Simulacionv1 extends JFrame{
 				boolean cellHasFocus) {
 			// TODO Intento de crear una label con la que interactuar
 			JLabel etiqueta = (JLabel)(super.getListCellRendererComponent(list, value, index, isSelected, cellHasFocus));
-			
+			JLabel nuevaEtiqueta = new JLabel();
+			if (value instanceof Planta) {
+	            Planta planta = (Planta) value;
+	            setText(planta.getNombre()); // solo muestra el nombre
+	            try {
+					setIcon(getIconoPlanta(planta));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} // le pongo un icono
+	        }
 			
 			return etiqueta;
 		}
+
+	}
+	private Icon getIconoPlanta(Planta planta) throws IOException {
+		// TODO dandole una planta me devulve su icono que deberia estar en imagenes
+		String[] posiblesplantas = {"Girasol", "Lanzaguisantes", "Hielaguisantes", "Apisonaflor", "Cactus", "Coltapulta", "Guisantralladora", "Humoseta", "Jalapeno", "Melonpulta","Nuez", "NuezGrande", "Patatapum", "PlantaCarronivora", "Repetidora", "SetaDesesporadora", "Trebolador", "Tripitidora" };
+		for (String nombreplanta : posiblesplantas) {
+			
+			try {
+				BufferedImage imagenLeer = ImageIO.read(new File("src/imagenes/" + planta.getTipo() + ".png"));
+				Image imagen = imagenLeer.getScaledInstance(24, 24, Image.SCALE_SMOOTH);
+				return new ImageIcon(imagen);
+				//return new ImageIcon("src/imagenes/" + planta.getTipo() + ".png");
+			} catch (Exception e) {
+				// TODO: handle exception
+				//return new ImageIcon("src/imagenes/NoIdentificada.png");
+				BufferedImage imagenLeer = ImageIO.read(new File("src/imagenes/NoIdentificada.png"));
+				Image imagen = imagenLeer.getScaledInstance(24, 24, Image.SCALE_SMOOTH);
+				return new ImageIcon(imagen);
+			}
+		}
+		return null;
 	}
 
-	String plantaSeleccionada = "";
+	Planta plantaSeleccionada = null;
 	
 	public Simulacionv1() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		setTitle("ventana de simulacion");
 		setSize(640, 480);
-		
+		/*
+		 * 
+		 */
 		String[] plantasBasicas = {
-				"Girasol","Lanzaguisantes","Nuez"
+				"Girasol","Lanzaguisantes","Nuez", "ojolera"
 		};
-		JList listaPlantas = new JList(plantasBasicas);
+		ArrayList<Planta> plantas = new ArrayList<Planta>();
+		MenuPlantas.cargarPlantasCSV(plantas);
+		DefaultListModel<Planta> modelo = new DefaultListModel<Planta>();
+		for(int i = 0; i<plantas.size();i++) {
+			modelo.add(i, plantas.get(i));
+		}
+		JList<Planta> listaPlantas = new JList<Planta>(modelo);
 		listaPlantas.setFixedCellWidth(200);
 		listaPlantas.setCellRenderer(new Mirenderizado());
 		listaPlantas.addListSelectionListener(new ListSelectionListener() {
@@ -57,8 +106,8 @@ public class Simulacionv1 extends JFrame{
 			public void valueChanged(ListSelectionEvent e) {
 				// TODO si se esta eligiendo una opcion superponer esa opcion en algun boton a selecionar
 				if(!e.getValueIsAdjusting()) {
-					System.out.println(listaPlantas.getSelectedValue().toString()+" ha sido seleccionada");
-					plantaSeleccionada = listaPlantas.getSelectedValue().toString();
+					System.out.println(listaPlantas.getSelectedValue().getNombre()+" ha sido seleccionada");
+					plantaSeleccionada = listaPlantas.getSelectedValue();
 				}
 			}
 		});
@@ -84,10 +133,16 @@ public class Simulacionv1 extends JFrame{
 				@Override
 				public void actionPerformed(ActionEvent e) {
 					// TODO funcion de interaccion del mouse 
-					if(plantaSeleccionada.equals("")) {
+					if(plantaSeleccionada.equals(null)) {
 						JOptionPane.showMessageDialog(Simulacionv1.this, "No has seleccionado una planta ha colocar", "Error", JOptionPane.ERROR_MESSAGE);
 					}else {
-						espacio.setText(plantaSeleccionada);						
+						espacio.setText(plantaSeleccionada.getNombre());
+						try {
+							espacio.setIcon(getIconoPlanta(plantaSeleccionada));
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
 					}
 				}
 			});
@@ -98,6 +153,8 @@ public class Simulacionv1 extends JFrame{
 		
 		
 		setVisible(true);
+		/*
+		 * 
 		for (int i = 0; i <= 100; i++) {
             barraProgreso.setValue(i); // Actualizar valor de la barra
             if(barraProgreso.isMaximumSizeSet()) {
@@ -109,5 +166,6 @@ public class Simulacionv1 extends JFrame{
                 e.printStackTrace();
             }
         }
+		 */
 	}
 }
