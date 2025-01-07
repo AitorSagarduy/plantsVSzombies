eca34e75d6953a26b1c156537c7a15673bfaf52f
r2213432 <jhojanmartinezmayta@gmail.com>
2025-01-07 19:46:37 +0100

    Avance de los zombies en base a su velocidad
    Tambien rango de las plantas aplicado

--- PvZ/src/gui/Batalla.java
+++ PvZ/src/gui/Batalla.java
@@ -1,15 +1,19 @@
 package gui;
 
+import java.awt.Graphics;
 import java.awt.GridLayout;
 import java.awt.Image;
+import java.io.File;
 import java.io.IOException;
-import java.nio.file.attribute.AclEntry;
 import java.util.ArrayList;
 import java.util.HashMap;
 
+import javax.imageio.ImageIO;
+import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 
 import domain.Planta;
@@ -20,16 +24,47 @@ public class Batalla extends JFrame implements Runnable{
 	private HashMap<ArrayList<Integer>, Planta> mapaFinalPlantas;
 	private HashMap<ArrayList<Integer>, Zombie> mapaFinalZombies;
 	
-	private boolean detener;
+	private static boolean detener;
 	public static void main(String[] args) {
 		new Simulacionv1();
 	}
+	public class BackgroundPanel extends JPanel {
+	    /**
+		 * 
+		 */
+		private static final long serialVersionUID = 1L;
+		private Image backgroundImage;
+
+	    public BackgroundPanel(String filePath) {
+	        try {
+	            backgroundImage = ImageIO.read(new File(filePath));
+	        } catch (IOException e) {
+	        }
+	    }
+
+	    @Override
+	    protected void paintComponent(Graphics g) {
+	        super.paintComponent(g);
+	        if (backgroundImage != null) {
+	            int width = getWidth();
+	            int height = getHeight();
+	            g.drawImage(backgroundImage, 0, 0, width, height, this);
+	        }
+	    }
+	}
+	
 	public Batalla(HashMap<ArrayList<Integer>, Planta> mapaFinal1, HashMap<ArrayList<Integer>, Zombie> mapaFinal2) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setTitle("ventana de batalla");
 		this.mapaFinalPlantas = mapaFinal1;
 		this.mapaFinalZombies = mapaFinal2;
-		setLayout(new GridLayout(5, 20));
+		
+		BackgroundPanel backgroundPanel = new BackgroundPanel("src/imagenes/PvZ-Patio3.JPG");
+        backgroundPanel.setLayout(new GridLayout(5, 20));
+        setContentPane(backgroundPanel);
+		//setLayout(new GridLayout(5, 20));
+		
+		
 		ArrayList<JButton> botones = new ArrayList<JButton>();
 		for(int i = 0; i<100;i++) {
 			int fila = i / 20; // 10 columnas por fila
@@ -37,20 +72,24 @@ public class Batalla extends JFrame implements Runnable{
 			JButton botonCesped = new JButton();
 			botonCesped.putClientProperty("fila", fila);
 			botonCesped.putClientProperty("columna", columna);
+			botonCesped.setBorder(BorderFactory.createEmptyBorder());
+			botonCesped.setFocusPainted(false);
+			botonCesped.setContentAreaFilled(false);
+			botonCesped.setOpaque(false);
 			ArrayList<Integer> coordenadas = new ArrayList<Integer>();
 		    coordenadas.add(fila);
 		    coordenadas.add(columna);
 		    
 		    if(mapaFinalPlantas.get(coordenadas) instanceof Planta) {
 		    	try {
-					botonCesped.setIcon(new ImageIcon(Simulacionv1.getBuferedimagePlanta(mapaFinalPlantas.get(coordenadas)).getScaledInstance(24, 24, Image.SCALE_SMOOTH)));
+					botonCesped.setIcon(new ImageIcon(Simulacionv1.getBuferedimagePlanta(mapaFinalPlantas.get(coordenadas)).getScaledInstance(Ajustes.resolucionx()/20, Ajustes.resoluciony()/5, Image.SCALE_SMOOTH)));
 					botonCesped.putClientProperty("planta", mapaFinalPlantas.get(coordenadas));
 		    	} catch (IOException e) {
 					e.printStackTrace();
 				}
 		    } else if (mapaFinalZombies.get(coordenadas) instanceof Zombie) {
 		    	try {
-					botonCesped.setIcon(new ImageIcon(Simulacionv2.getBuferedimagePlanta(mapaFinalZombies.get(coordenadas)).getScaledInstance(24, 24, Image.SCALE_SMOOTH)));
+					botonCesped.setIcon(new ImageIcon(Simulacionv2.getBuferedimagePlanta(mapaFinalZombies.get(coordenadas)).getScaledInstance(Ajustes.resolucionx()/20, Ajustes.resoluciony()/5, Image.SCALE_SMOOTH)));
 					botonCesped.putClientProperty("planta", mapaFinalZombies.get(coordenadas));
 		    	} catch (IOException e) {
 					e.printStackTrace();
@@ -81,16 +120,7 @@ public class Batalla extends JFrame implements Runnable{
 				int finalII = i;
 				hilo = new Thread(() -> {
 					int finalI = finalII;
-					while (!detener) {
-						if(finalI%20 == 0) {
-							detener = true;
-							System.out.println("Los zombies ganan");
-						}
-                        try {
-                            Thread.sleep(zombie.getVelocidad()*1000);
-                        } catch (InterruptedException e) {
-                            e.printStackTrace();
-                        }
+					while (!detener && zombie.getVida() > 0) {
                         if(botones.get(finalI-1).getClientProperty("planta") instanceof Planta) {
                         	Planta planta = (Planta) botones.get(finalI-1).getClientProperty("planta");
                         	planta.setVida(planta.getVida()-zombie.getDanyo());
@@ -103,11 +133,16 @@ public class Batalla extends JFrame implements Runnable{
 								botones.get(finalI).putClientProperty("planta", null);
 								finalI--;
 							}
+							System.out.println ("Zombie ataca");
+							try {
+								Thread.sleep(zombie.getTmp_atac()*1000);
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
                         } else if (botones.get(finalI-1).getClientProperty("planta") instanceof Zombie) {
                         	try {
-								hilo.sleep(100);
+								Thread.sleep(10);
 							} catch (InterruptedException e) {
-								// TODO Auto-generated catch block
 								e.printStackTrace();
 							}
                         } else {
@@ -116,8 +151,16 @@ public class Batalla extends JFrame implements Runnable{
                         	botones.get(finalI).setIcon(null);
                         	botones.get(finalI).putClientProperty("planta", null);
                         	finalI--;
+                        	try {
+                                Thread.sleep((zombie.getVelocidad())*1000);
+                            } catch (InterruptedException e) {
+                                e.printStackTrace();
+                            }
                         }
-                        
+                        if(finalI%20 == 0) {
+							detener = true;
+							System.out.println("Los zombies ganan");
+						}
                     }
 				});
 				hilosZombies.add(hilo);
@@ -131,11 +174,6 @@ public class Batalla extends JFrame implements Runnable{
 				hilo = new Thread(() -> {
 					int finalI = finalII;
 					while (!detener) {
-						try {
-							Thread.sleep(planta.getTmp_atac() * 1000);
-						} catch (InterruptedException e) {
-							e.printStackTrace();
-						}
 						int numeroZomb = 0;
 						for (int j = 0; j < 100; j++) {
 							if (botones.get(j).getClientProperty("planta") instanceof Zombie) {
@@ -146,7 +184,7 @@ public class Batalla extends JFrame implements Runnable{
 							detener = true;
 							System.out.println("Las plantas ganan");
 						}else{
-							for(int u = finalI ; u%20 <= 19;u++ ){
+							for(int u = finalI ; u%19 <= finalI%20+planta.getRango();u++ ){
 								if(botones.get(u).getClientProperty("planta") instanceof Zombie) {
 									System.out.println("Planta ataca a zombie en la columna: "+u%20);	
 									Zombie zombie = (Zombie) botones.get(u).getClientProperty("planta");
@@ -157,7 +195,7 @@ public class Batalla extends JFrame implements Runnable{
 											((Zombie)botones.get(u-1).getClientProperty("planta")).setVida(((Zombie)botones.get(u-1).getClientProperty("planta")).getVida()-planta.getDanyo());
                                             
                                         } catch (Exception e2) {
-                                        	
+                                        	//aqui solamente llega si es que la planta muere en el instante de atacar
                                         }
 									}
 									try {
@@ -166,11 +204,17 @@ public class Batalla extends JFrame implements Runnable{
 											botones.get(u).putClientProperty("planta", null);
 										}
 									} catch (Exception e) {
-										// TODO: handle exception
-										if(((Zombie)botones.get(u-1).getClientProperty("planta")).getVida()<=0) {
-											botones.get(u-1).setIcon(null);
-											botones.get(u-1).putClientProperty("planta", null);
+										if(botones.get(u).getClientProperty("planta") instanceof Zombie) {
+											if(((Zombie)botones.get(u-1).getClientProperty("planta")).getVida()<=0) {
+												botones.get(u-1).setIcon(null);
+												botones.get(u-1).putClientProperty("planta", null);
+											}
 										}
+									}
+									try {
+										Thread.sleep(planta.getTmp_atac() * 1000);
+									} catch (InterruptedException e) {
+										e.printStackTrace();
 									}
 									break;
 								}
@@ -181,12 +225,24 @@ public class Batalla extends JFrame implements Runnable{
 				hilosPlantas.add(hilo);
 			}
 		}
+		Thread hilo = new Thread();
+		hilo = new Thread(() -> {
+			if (detener) {
+                for (Thread thread : hilosZombies) {
+                    thread.interrupt();
+                }
+                for (Thread thread : hilosPlantas) {
+                    thread.interrupt();
+                }
+            }
+		});
 		for (Thread thread : hilosZombies) {
 			thread.start();
 		}
 		for (Thread thread : hilosPlantas) {
 			thread.start();
 		}
+		hilo.start();
 	}
 	@Override
 	public void run() {
